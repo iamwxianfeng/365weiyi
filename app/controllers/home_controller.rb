1ee6d5e170c85e97a01e76fc8c4dca9cde4c0f53
wxianfeng <wang.fl1429@gmail.com>
2013-09-27 00:36:35 +0800

    home index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,12 @@ class HomeController < ApplicationController
   layout false
 
   def index
-
+    banners = Banner.all(:order => 'popularity DESC, created_at DESC')
+    @scrolling_banners = banners.find_all {|i| i.can_scroll == true and i.popularity < Banner::TOP_BANNER}
+    @top_banners = banners.find_all {|i| i.popularity == Banner::TOP_BANNER}
+    @side_banners = banners.find_all {|i| i.popularity < Banner::TOP_BANNER and i.can_scroll == false}
+    @top_rulers = TopRuler.includes("user").limit(2)
+    @posts = BlogPost.limit(7).order("created_at DESC")
   end
 
 end
