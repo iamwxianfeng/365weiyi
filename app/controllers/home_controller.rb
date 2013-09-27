ec601dc862d539d26f4510605c732215060acaac
wxianfeng <wang.fl1429@gmail.com>
2013-09-28 00:10:42 +0800

    suit product

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,6 +11,8 @@ class HomeController < ApplicationController
     @side_banners = banners.find_all {|i| i.popularity < Banner::TOP_BANNER and i.can_scroll == false}
     @top_rulers = TopRuler.includes("user").limit(2)
     @posts = BlogPost.limit(7).order("created_at DESC")
+    suit_category = Category.find_by_name("西服定制")
+    @suit_products = suit_category.products.order('popularity DESC,our_price ASC').limit(5)
   end
 
 end
