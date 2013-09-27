0a527cf430a19960bd9e95eef98be599c58f857f
wxianfeng <wang.fl1429@gmail.com>
2013-09-27 23:27:01 +0800

    slide image

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   layout false
 
   def index
-    banners = Banner.all(:order => 'popularity DESC, created_at DESC')
+    banners = Banner.all(:order => 'popularity DESC, created_at DESC',:conditions=>"popularity < 10")
     @scrolling_banners = banners.find_all {|i| i.can_scroll == true and i.popularity < Banner::TOP_BANNER}
     @top_banners = banners.find_all {|i| i.popularity == Banner::TOP_BANNER}
     @side_banners = banners.find_all {|i| i.popularity < Banner::TOP_BANNER and i.can_scroll == false}
