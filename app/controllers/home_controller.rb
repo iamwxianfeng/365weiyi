5bad3611f8fe6ac4ed6f145fad172341a9a1d82d
wxianfeng <wang.fl1429@gmail.com>
2013-09-28 00:45:09 +0800

    shirt product

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,13 +7,17 @@ class HomeController < ApplicationController
     banners = Banner.all(:order => 'popularity DESC, created_at DESC',:conditions=>"popularity < 10")
     @scrolling_banners = banners.find_all {|i| i.can_scroll == true and i.popularity < Banner::TOP_BANNER}
     @suit_banner = Banner.where(:popularity=>Banner::HOME_SUIT).order("created_at ASC").last
+    @shirt_banner = Banner.where(:popularity=>Banner::HOME_SHIRT).order("created_at ASC").last
     @top_banners = banners.find_all {|i| i.popularity == Banner::TOP_BANNER}
     @side_banners = banners.find_all {|i| i.popularity < Banner::TOP_BANNER and i.can_scroll == false}
     @top_rulers = TopRuler.includes("user").limit(2)
     @posts = BlogPost.limit(7).order("created_at DESC")
     suit_category = Category.find_by_name("西服定制")
     @suit_products = suit_category.products.order('popularity DESC,our_price ASC').limit(5)
+    shirt_category = Category.find_by_name("衬衫定制")
+    @shirt_products  = shirt_category.products.order('popularity DESC,our_price ASC').limit(5)
     @brand_suits = Banner.where(:popularity=>Banner::BRAND_SUIT).order("created_at DESC").limit(4)
+    @brand_shirts = Banner.where(:popularity=>Banner::BRAND_SHIRT).order("created_at DESC").limit(4)
   end
 
 end
