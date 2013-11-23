8b0866e886f37ff1d4881e3f2d912f60ad5fe933
wxianfeng <wang.fl1429@gmail.com>
2013-11-23 14:55:21 +0800

    fix nil

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -32,17 +32,17 @@ class CustomersController < ApplicationController
       else
         @customers.first
       end
-      @shirt_product1 = Product.find @item.shirt_product1
-      @shirt_product2 = Product.find @item.shirt_product2
-      @shirt_product3 = Product.find @item.shirt_product3
+      @shirt_product1 = Product.find_by_id @item.shirt_product1
+      @shirt_product2 = Product.find_by_id @item.shirt_product2
+      @shirt_product3 = Product.find_by_id @item.shirt_product3
 
-      @suit_product1 = Product.find @item.suit_product1
-      @suit_product2 = Product.find @item.suit_product2
-      @suit_product3 = Product.find @item.suit_product3
+      @suit_product1 = Product.find_by_id @item.suit_product1
+      @suit_product2 = Product.find_by_id @item.suit_product2
+      @suit_product3 = Product.find_by_id @item.suit_product3
 
-      @other_product1 = Product.find @item.other_product1
-      @other_product2 = Product.find @item.other_product2
-      @other_product3 = Product.find @item.other_product3
+      @other_product1 = Product.find_by_id @item.other_product1
+      @other_product2 = Product.find_by_id @item.other_product2
+      @other_product3 = Product.find_by_id @item.other_product3
     end
     if request.xhr?
       render :partial => "content"
